c05104c748104296a0bf72c8161c9144322be45d
coord.e <me@coord-e.com>
2018-03-17 07:57:03 +0900

    Add train script

---
+++ train.py
@@ -0,0 +1,42 @@
+#add parent dir to find package. Only needed for source code build, pip install doesn't need it.
+import os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+os.sys.path.insert(0,parentdir)
+
+import gym
+from yamaxenv import YamaXEnv
+
+from baselines.ppo1 import mlp_policy, pposgd_simple
+from baselines.common import tf_util as U
+
+from baselines import logger
+
+import pybullet as p
+
+def callback(lcl, glb):
+    # stop training if reward exceeds 199
+    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+    return is_solved
+
+def main():
+    logger.configure()
+    U.make_session().__enter__()
+    env = YamaXEnv(renders=True)
+    def policy_fn(name, ob_space, ac_space):
+        return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+            hid_size=64, num_hid_layers=2)
+
+    act = pposgd_simple.learn(env, policy_fn,
+            max_timesteps=10000000,
+            timesteps_per_actorbatch=2048,
+            clip_param=0.2, entcoeff=0.0,
+            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
+            gamma=0.99, lam=0.95, schedule='linear',
+        )
+    print("Saving model to cartpole_model.pkl")
+    act.save("cartpole_model.pkl")
+
+
+if __name__ == '__main__':
+    main()
