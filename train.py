da1f9b2be51652417c055cd14b0271b9a1a7aa92
coord.e <me@coord-e.com>
2018-03-21 13:41:47 +0900

    Rewrite train.py based on tensorforce's example

--- train.py
+++ train.py
@@ -1,46 +1,169 @@
-#add parent dir to find package. Only needed for source code build, pip install doesn't need it.
-import os, inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(os.path.dirname(currentdir))
-os.sys.path.insert(0,parentdir)
+# Copyright 2017 reinforce.io. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-import gym
+"""
+OpenAI gym execution.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import json
+import logging
+import os
+import time
+
+from tensorforce import TensorForceError
+from tensorforce.agents import Agent
+from tensorforce.execution import Runner
+from tensorforce.contrib.openai_gym import OpenAIGym
+
 from yamaxenv import YamaXEnv
+# python examples/openai_gym.py Pong-ram-v0 -a examples/configs/vpg.json -n examples/configs/mlp2_network.json -e 50000 -m 2000
 
-from baselines.ppo1 import mlp_policy, pposgd_simple
-from baselines.common import tf_util as U
-from baselines import bench
-from baselines import logger
+# python examples/openai_gym.py CartPole-v0 -a examples/configs/vpg.json -n examples/configs/mlp2_network.json -e 2000 -m 200
 
-import tensorflow as tf
-
-import pybullet as p
-
-def callback(lcl, glb):
-    # stop training if reward exceeds 199
-    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
-    return is_solved
 
 def main():
-    logger.configure()
-    sess = U.make_session()
-    sess.__enter__()
-    logger.configure()
-    env = bench.Monitor(YamaXEnv(logfile="log.csv", renders=False), "log/monitor.json")
-    def policy_fn(name, ob_space, ac_space):
-        return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
-            hid_size=64, num_hid_layers=2)
+    parser = argparse.ArgumentParser()
 
-    act = pposgd_simple.learn(env, policy_fn,
-            max_timesteps=10000000,
-            timesteps_per_actorbatch=2048,
-            clip_param=0.2, entcoeff=0.0,
-            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
-            gamma=0.99, lam=0.95, schedule='linear',
+    parser.add_argument('-a', '--agent', help="Agent configuration file")
+    parser.add_argument('-n', '--network', default=None, help="Network specification file")
+    parser.add_argument('-e', '--episodes', type=int, default=None, help="Number of episodes")
+    parser.add_argument('-t', '--timesteps', type=int, default=None, help="Number of timesteps")
+    parser.add_argument('-m', '--max-episode-timesteps', type=int, default=None, help="Maximum number of timesteps per episode")
+    parser.add_argument('-d', '--deterministic', action='store_true', default=False, help="Choose actions deterministically")
+    parser.add_argument('-s', '--save', help="Save agent to this dir")
+    parser.add_argument('-se', '--save-episodes', type=int, default=100, help="Save agent every x episodes")
+    parser.add_argument('-l', '--load', help="Load agent from this dir")
+    parser.add_argument('--monitor', help="Save results to this directory")
+    parser.add_argument('--monitor-safe', action='store_true', default=False, help="Do not overwrite previous results")
+    parser.add_argument('--monitor-video', type=int, default=0, help="Save video every x steps (0 = disabled)")
+    parser.add_argument('--visualize', action='store_true', default=False, help="Enable OpenAI Gym's visualization")
+    parser.add_argument('-D', '--debug', action='store_true', default=False, help="Show debug outputs")
+    parser.add_argument('--job', type=str, default=None, help="For distributed mode: The job type of this agent.")
+    parser.add_argument('--task', type=int, default=0, help="For distributed mode: The task index of this agent.")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    logger = logging.getLogger(__file__)
+    logger.setLevel(logging.INFO)
+
+    environment = OpenAIGym.__new__(OpenAIGym)
+    environment.gym_id = "YamaXEnv-v0"
+    environment.gym = YamaXEnv(renders=args.visualize)
+    environment.visualize = args.visualize
+
+    if args.agent is not None:
+        with open(args.agent, 'r') as fp:
+            agent = json.load(fp=fp)
+    else:
+        raise TensorForceError("No agent configuration provided.")
+
+    if args.network is not None:
+        with open(args.network, 'r') as fp:
+            network = json.load(fp=fp)
+    else:
+        network = None
+        logger.info("No network configuration provided.")
+
+    # TEST
+    agent["execution"] = dict(
+        type="distributed",
+        distributed_spec=dict(
+            job=args.job,
+            task_index=args.task,
+            # parameter_server=(args.job == "ps"),
+            cluster_spec=dict(
+                ps=["192.168.2.107:22222"],
+                worker=["192.168.2.107:22223"]
+            ))
+    ) if args.job else None
+    # END: TEST
+
+    agent = Agent.from_spec(
+        spec=agent,
+        kwargs=dict(
+            states=environment.states,
+            actions=environment.actions,
+            network=network,
         )
-    env.close()
-    saver = tf.train.Saver()
-    saver.save(sess, './model/deepl2_10000000')
+    )
+    if args.load:
+        load_dir = os.path.dirname(args.load)
+        if not os.path.isdir(load_dir):
+            raise OSError("Could not load agent from {}: No such directory.".format(load_dir))
+        agent.restore_model(args.load)
+
+    if args.save:
+        save_dir = os.path.dirname(args.save)
+        if not os.path.isdir(save_dir):
+            try:
+                os.mkdir(save_dir, 0o755)
+            except OSError:
+                raise OSError("Cannot save agent to dir {} ()".format(save_dir))
+
+    if args.debug:
+        logger.info("-" * 16)
+        logger.info("Configuration:")
+        logger.info(agent)
+
+    runner = Runner(
+        agent=agent,
+        environment=environment,
+        repeat_actions=1
+    )
+
+    if args.debug:  # TODO: Timestep-based reporting
+        report_episodes = 1
+    else:
+        report_episodes = 100
+
+    logger.info("Starting {agent} for Environment '{env}'".format(agent=agent, env=environment))
+
+    def episode_finished(r, id_):
+        if r.episode % report_episodes == 0:
+            steps_per_second = r.timestep / (time.time() - r.start_time)
+            logger.info("Finished episode {:d} after {:d} timesteps. Steps Per Second {:0.2f}".format(
+                r.agent.episode, r.episode_timestep, steps_per_second
+            ))
+            logger.info("Episode reward: {}".format(r.episode_rewards[-1]))
+            logger.info("Average of last 500 rewards: {:0.2f}".
+                        format(sum(r.episode_rewards[-500:]) / min(500, len(r.episode_rewards))))
+            logger.info("Average of last 100 rewards: {:0.2f}".
+                        format(sum(r.episode_rewards[-100:]) / min(100, len(r.episode_rewards))))
+            if args.save and args.save_episodes is not None and not r.episode % args.save_episodes:
+                logger.info("Saving agent to {}".format(args.save))
+                r.agent.save_model(args.save)
+        return True
+
+    runner.run(
+        num_timesteps=args.timesteps,
+        num_episodes=args.episodes,
+        max_episode_timesteps=args.max_episode_timesteps,
+        deterministic=args.deterministic,
+        episode_finished=episode_finished
+    )
+    runner.close()
+
+    logger.info("Learning finished. Total episodes: {ep}".format(ep=runner.agent.episode))
+
 
 if __name__ == '__main__':
     main()
