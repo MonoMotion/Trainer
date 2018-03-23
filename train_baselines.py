4f71e001d6d3cd21c310943b38fdb0b53b5d2f69
coord.e <me@coord-e.com>
2018-03-23 21:08:25 +0900

    Add arguments to train script

--- train_baselines.py
+++ train_baselines.py
@@ -5,6 +5,7 @@ parentdir = os.path.dirname(os.path.dirname(currentdir))
 os.sys.path.insert(0,parentdir)
 
 import gym
+import argparse
 from yamaxenv import YamaXEnv
 
 from baselines.ppo1 import mlp_policy, pposgd_simple
@@ -17,17 +18,32 @@ import tensorflow as tf
 import pybullet as p
 
 def main():
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument('-e', '--episodes', type=int, default=None, help="Number of episodes")
+    parser.add_argument('-t', '--timesteps', type=int, default=None, help="Number of timesteps")
+    # parser.add_argument('-m', '--max-episode-timesteps', type=int, default=None, help="Maximum number of timesteps per episode")
+    parser.add_argument('-s', '--save', help="Save agent to this dir")
+    # parser.add_argument('-se', '--save-episodes', type=int, default=5000, help="Save agent every x episodes")
+    # parser.add_argument('-l', '--load', help="Load agent from this dir")
+    parser.add_argument('--monitor', help="Save results to this directory")
+    # parser.add_argument('--monitor-safe', action='store_true', default=False, help="Do not overwrite previous results")
+    # parser.add_argument('--monitor-video', type=int, default=5000, help="Save video every x steps (0 = disabled)")
+    parser.add_argument('--visualize', action='store_true', default=False, help="Enable OpenAI Gym's visualization")
+
+    args = parser.parse_args()
+
     logger.configure()
     sess = U.make_session()
     sess.__enter__()
     logger.configure()
-    env = bench.Monitor(YamaXEnv(logdir="./monitor/", renders=False), "./monitor/monitor.json")
+    env = bench.Monitor(YamaXEnv(logdir=args.monitor, renders=args.visualize), args.monitor)
     def policy_fn(name, ob_space, ac_space):
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
             hid_size=64, num_hid_layers=2)
 
     act = pposgd_simple.learn(env, policy_fn,
-            max_timesteps=10000000,
+            max_timesteps=args.timesteps,
             timesteps_per_actorbatch=2048,
             clip_param=0.2, entcoeff=0.0,
             optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
@@ -35,7 +51,7 @@ def main():
         )
     env.close()
     saver = tf.train.Saver()
-    saver.save(sess, './model/deepl2_10000000')
+    saver.save(sess, args.save)
 
 if __name__ == '__main__':
     main()
