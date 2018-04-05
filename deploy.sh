f522097ef4b4d32364b3ce5991d7b0fbb0951acf
coord.e <me@coord-e.com>
2018-04-06 00:59:48 +0900

    Join commands

--- deploy.sh
+++ deploy.sh
@@ -36,16 +36,16 @@ done
 scp -o StrictHostKeychecking=no -r -i ~/.ssh/terraform [!.]* ubuntu@$IP_ADDR:/home/ubuntu/deepl2
 
 ssh -t -t -o StrictHostKeychecking=no -i ~/.ssh/terraform ubuntu@$IP_ADDR << EOS
-export DEBIAN_FRONTEND=noninteractive
-cd /home/ubuntu/deepl2
-sudo -E apt-get update
-sudo -E apt-get install -y -qq ffmpeg python3-pip python3-tk libffi-dev libopenmpi-dev libssl-dev psmisc curl git
-sudo -E pip3 install pipenv
-git clone https://github.com/openai/baselines --depth 1
-sed -i -e 's/mujoco,atari,classic_control,robotics/classic_control/g' baselines/setup.py
-pipenv install baselines/
-pipenv install
-. creds.sh
-./run.sh
-exit
+export DEBIAN_FRONTEND=noninteractive \
+&& cd /home/ubuntu/deepl2 \
+&& sudo -E apt-get update \
+&& sudo -E apt-get install -y -qq ffmpeg python3-pip python3-tk libffi-dev libopenmpi-dev libssl-dev psmisc curl git \
+&& sudo -E pip3 install pipenv \
+&& git clone https://github.com/openai/baselines --depth 1 \
+&& sed -i -e 's/mujoco,atari,classic_control,robotics/classic_control/g' baselines/setup.py \
+&& pipenv install baselines/ \
+&& pipenv install \
+&& . creds.sh \
+&& . run.sh \
+&& exit
 EOS
