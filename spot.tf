cbb085912a04f33efedbc3f23b4f9a1946d8bc85
coord.e <me@coord-e.com>
2018-04-05 23:38:42 +0900

    Add egress

--- spot.tf
+++ spot.tf
@@ -36,6 +36,13 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "auth" {
