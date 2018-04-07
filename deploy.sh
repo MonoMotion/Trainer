225fafc98c521752a48932a93dab1c6546f0f528
coord.e <me@coord-e.com>
2018-04-07 19:32:15 +0900

    Still trying to supply aws creds to tf's s3 plugin

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,7 @@ export DEEPL2_DISCORD_TOKEN='${DEEPL2_DISCORD_TOKEN}'
 export DEEPL2_DISCORD_CHANNEL='${DEEPL2_DISCORD_CHANNEL}'
 EOS
 
-cat << EOS > ~/.aws/credentials
-[default]
-aws_access_key_id = ${TF_VAR_access_key}
-aws_secret_access_key = ${TF_VAR_secret_key}
-EOS
+. creds.sh
 
 terraform init -input=false
 terraform plan -out=tfplan -input=false
