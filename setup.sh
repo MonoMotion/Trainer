efb305ff19f1d486861f85bbe1d0f678b102ad97
coord.e <me@coord-e.com>
2018-09-19 17:11:25 +0900

    Add: Add a setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ROBOSCHOOL_PATH=./roboschool
+
+mkdir bullet3/build
+cd    bullet3/build
+cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$ROBOSCHOOL_PATH/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
+make -j"$(nproc)"
+make install
