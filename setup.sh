f5bf22f47d10ee00d0592fa1028cd4df5695633f
coord.e <me@coord-e.com>
2018-09-19 23:12:14 +0900

    Fix: Link from current directory

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ if [ ! -d $ROBOSCHOOL_PATH/roboschool ]; then
 fi
 
 mkdir -p bullet3/build
-cd    bullet3/build
+pushd    bullet3/build
 cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$ROBOSCHOOL_PATH/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
 make -j"$(nproc)"
 make install
+popd
 
 ln -vsr robot_models $ROBOSCHOOL_PATH/roboschool/models_robot/robot_models
 
