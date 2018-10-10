a0ea3875935ad4295faeee3a0083393b7099d0ce
coord.e <me@coord-e.com>
2018-10-11 00:42:10 +0900

    Fix: Improve some in setup script

--- setup.sh
+++ setup.sh
@@ -2,29 +2,34 @@
 
 set -eu
 
-ROBOSCHOOL_PATH=$(realpath ./roboschool)
-
-if [ ! -d $ROBOSCHOOL_PATH/roboschool ]; then
-  git submodule update --init --recursive
-fi
-
+# Bullet installation for roboschool
+roboschool_path=$(realpath ./roboschool)
 mkdir -p bullet3/build
 pushd    bullet3/build
-cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$ROBOSCHOOL_PATH/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
+cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
 make -j"$(nproc)"
 make install
 popd
 
+# Include out model to the search path
 ln -vsr robot_models $ROBOSCHOOL_PATH/roboschool/models_robot/robot_models
 
+# Download and patch YamaX model
 wget -O robot_models/yamax.urdf https://github.com/Y-modify/YamaX/releases/download/4.0/YamaX_4.0.urdf
 patch robot_models/yamax.urdf < yamax.urdf.patch
 
 if type "nvidia-smi" > /dev/null 2>&1
 then
-  sed -i 's/tensorflow = "\*"/tensorflow-gpu = "*"/' Pipfile
+  sed -i 's/tensorflow =/tensorflow-gpu =/' Pipfile
 else
   echo "Using tensorflow without GPU support"
 fi
 
-pipenv install
+# Roboschool installation needs to be done in virtualenv
+if [ -f Pipfile.lock ]; then
+  pipenv run pipenv install
+else
+  pipenv install
+  pipenv install -e ./baselines
+  pipenv run pipenv install -e ./roboschool
+fi
