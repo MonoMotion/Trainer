0d23f01a1d0b19b8506fcdc60d96a0d801854d89
coord.e <me@coord-e.com>
2018-11-27 20:37:16 +0900

    Fix: FIx plot utility to treat json motion file

--- utils/plot_motion.py
+++ utils/plot_motion.py
@@ -1,21 +1,25 @@
 from argparse import ArgumentParser
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 parser = ArgumentParser(description='Plot motion file')
 parser.add_argument('input', type=str, help='Input motion file')
 parser.add_argument('-o', '--output', type=str, help='output plot file', required=True)
 args = parser.parse_args()
 
-data = np.loadtxt(args.input, delimiter=',')
-num_joints = int(data.T[1].max())
+with open(args.input) as f:
+    data = json.load(f)
+num_joints = data['num_joints']
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-for i in range(num_joints):
-    x, y = np.array([[d[0], d[2]] for d in data if int(d[1] == i)]).T
-    assert len(x) == len(y)
+seq = data['sequence']
+
+x = list(map(float, seq.keys()))
+ys = np.array(list(seq.values())).T
+for (i, y) in enumerate(ys):
     ax.plot(x, y, label=str(i))
 
 ax.legend()
