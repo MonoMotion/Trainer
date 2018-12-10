ea6b00c6e7825b729d16dba35b8359771ef04154
coord.e <me@coord-e.com>
2018-12-10 07:57:14 +0000

    Fix: Fix parameters

--- gym_yamax/motion.py
+++ gym_yamax/motion.py
@@ -73,13 +73,13 @@ class EffectorTarget(object):
 
     def interp(self, t, t1, t2, target):
         if self.location is not None:
-            location = self.location.interp(target.location)
+            location = self.location.interp(t, t1, t2, target.location)
         else:
             assert target.location is None
             location = None
 
         if self.rotation is not None:
-            rotation = self.rotation.interp(target.rotation)
+            rotation = self.rotation.interp(t, t1, t2, target.rotation)
         else:
             assert target.rotation is None
             rotation = None
@@ -106,7 +106,7 @@ def get_frame_at(t, motion_iter):
         positions = {j: simple_interp(t, t1, t2, p1, p2) for j, (p1, p2) in dictzip(p1, p2)}
 
         def conv(eff):
-            return {l: make_effector_target(e1) for l, e in eff.items()}
+            return {l: make_effector_target(e) for l, e in eff.items()}
 
         effectors = {l: e1.interp(t, t1, t2, e2) for l, (e1, e2) in dictzip(conv(e1), conv(e2))}
 
