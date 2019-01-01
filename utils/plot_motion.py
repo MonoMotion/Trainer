a8a577a98faa9ccf869cf9c6275ed40a6d904f87
coord.e <me@coord-e.com>
2019-01-01 12:47:59 +0000

    Change: Use flom in plot_motion util

--- utils/plot_motion.py
+++ utils/plot_motion.py
@@ -2,43 +2,29 @@ from argparse import ArgumentParser
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-import json
+import flom
 
 parser = ArgumentParser(description='Plot motion file')
 parser.add_argument('input', type=str, help='Input motion file')
-parser.add_argument('-l', '--loop', type=int, default=2,
-                    help='The number of repetitions when loop == wrap')
+parser.add_argument('--fps', type=float, default=0.01)
 parser.add_argument('-o', '--output', type=str, help='output plot file', required=True)
 args = parser.parse_args()
 
-with open(args.input) as f:
-    data = json.load(f)
+motion = flom.load(args.input)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-loop = data['loop']
-frames = data['frames']
-joints = [j for j in frames[0]['position'].keys() if not j.startswith('ignore_')]
+joints = [j for j in motion.joint_names() if not j.startswith('ignore_')]
 
-
-def plot_frames(frames):
-    x = [frame['timepoint'] for frame in frames]
-    ys = [frame['position'] for frame in frames]
+def plot_frames(motion):
+    motion.set_loop(flom.LoopType.None_)
+    x = [i * args.fps for i, frame in enumerate(motion.frames(args.fps))]
+    ys = [frame.positions for frame in motion.frames(args.fps)]
     for joint in joints:
         ax.plot(x, [y[joint] for y in ys], label=joint)
 
-
-if loop == 'none':
-    plot_frames(frames)
-elif loop == 'wrap':
-    looped = frames
-    for _ in range(args.loop - 1):
-        looped = looped + [{'timepoint': frame['timepoint'] + looped[-1]
-                            ['timepoint'], 'position': frame['position']} for frame in frames]
-    plot_frames(looped)
-else:
-    sys.exit("invalid loop value: {}".format(loop))
+plot_frames(motion)
 
 ax.legend()
 plt.savefig(args.output)
