6203285095dee164c76c581115e0f4dcc998a05e
coord.e <me@coord-e.com>
2019-01-02 03:57:49 +0000

    Change: Move bullet and roboschool to third_party/

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 set -eu
 
 # Bullet installation for roboschool
-roboschool_path=$(realpath ./roboschool)
-mkdir -p bullet3/build
-pushd    bullet3/build
+roboschool_path=$(realpath ./third_party/roboschool)
+mkdir -p third_party/bullet3/build
+pushd    third_party/bullet3/build
 cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..  make -j"$(nproc)" make install
 popd
 
