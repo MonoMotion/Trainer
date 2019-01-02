4da8d64457a3d4538a734146573ad4359b5c8fa8
coord.e <me@coord-e.com>
2019-01-02 06:46:18 +0000

    Fix: Add necessary new lines

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 
 set -euo pipefail
 
-
 function build_bullet3() {
   local roboschool_path=$(realpath ./third_party/roboschool)
   # Bullet installation for roboschool
   mkdir -p third_party/bullet3/build
   pushd    third_party/bullet3/build
-  cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..  make -j"$(nproc)" make install
+  cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
+  make -j"$(nproc)"
+  make install
   popd
 }
 
