98ad4e419283a8695de5a48a2a356ad6e2218ad9
coord.e <me@coord-e.com>
2019-01-02 05:33:47 +0000

    Add: Use locally-installed boost.python

--- setup.sh
+++ setup.sh
@@ -9,6 +9,18 @@ pushd    third_party/bullet3/build
 cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..  make -j"$(nproc)" make install
 popd
 
+mv Pipfile Pipfile.bak
+sed '/roboschool = /d' Pipfile.bak > Pipfile
+pipenv install --skip-lock
+mv Pipfile.bak Pipfile
+
+pushd third_party/boost-python
+pipenv run faber
+popd
+
+boost_python_lib="$(realpath $(find third_party/boost-python -name '*boost_python*.so'))"
+boost_python_include="$(realpath third_party/boost-python/include)"
+
 # Include out model to the search path
 if [ ! -e $roboschool_path/roboschool/models_robot/robot_models ]; then
   ln -vsr robot_models $roboschool_path/roboschool/models_robot/robot_models
@@ -19,10 +31,5 @@ wget -O robot_models/yamax.urdf https://github.com/Y-modify/YamaX/releases/downl
 patch robot_models/yamax.urdf < yamax.urdf.patch
 
 # Roboschool installation needs to be done in virtualenv
-if [ -f Pipfile.lock ]; then
-  pipenv run pipenv install
-else
-  pipenv install
-  PYTHON_VERSION=$(pipenv run python -V | awk '{print $2}')
-  PKG_CONFIG_PATH=$HOME/.pyenv/versions/$PYTHON_VERSION/lib/pkgconfig pipenv run pipenv install -e ./roboschool
-fi
+PYTHON_VERSION=$(pipenv run python -V | awk '{print $2}')
+PKG_CONFIG_PATH=$HOME/.pyenv/versions/$PYTHON_VERSION/lib/pkgconfig CPLUS_INCLUDE_PATH=$boost_python_include LIBRARY_PATH="$(dirname $boost_python_lib)" pipenv run pipenv install
