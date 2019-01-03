d3e23679aa7a4e35f5a094fcbcfa001a9b6ecbdc
coord.e <me@coord-e.com>
2019-01-03 02:35:51 +0000

    Add: Render as effector position changes

--- trainer/preview.py
+++ trainer/preview.py
@@ -1,25 +1,28 @@
 from .simulation import create_scene, reset, apply_joints, render
 
-EFFECTOR_SPHERE_RADIUS = 0.05
+EFFECTOR_SPHERE_RADIUS_RATIO = 0.05
 EFFECTOR_SPHERE_COLOR = 0x2ecc77
 
-def create_effectors(scene, parts, motion):
-    def create(name):
-        x, y, z = parts[name].pose().xyz()
-        return scene.cpp_world.debug_sphere(x, y, z, EFFECTOR_SPHERE_RADIUS, EFFECTOR_SPHERE_COLOR)
+def create_effector_visualizer(scene, effectors):
+    def create(eff):
+        if eff.location:
+            x, y, z = eff.location.vec
+            radius = eff.location.weight * EFFECTOR_SPHERE_RADIUS_RATIO
+            return scene.cpp_world.debug_sphere(x, y, z, radius, EFFECTOR_SPHERE_COLOR)
 
-    return {name: create(name) for name in motion.effector_names()}
+    return {name: create(eff) for name, eff in effectors.items()}
 
 def preview(motion, robot_file, timestep=0.0165/8, frame_skip=8):
     scene = create_scene(timestep, frame_skip)
 
     robot, parts, joints = reset(scene, robot_file)
-    effectors = create_effectors(scene, parts, motion)
 
+    effector_vis = None
     while True:
         scene.global_step()
 
         frame = motion.frame_at(scene.cpp_world.ts)
         apply_joints(joints, frame.positions)
+        effector_vis = create_effector_visualizer(scene, frame.effectors)
 
         render(scene)
