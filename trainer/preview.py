a13441bfefab76c334ac560e22ffd1d0a3e061e5
coord.e <me@coord-e.com>
2019-01-03 05:23:37 +0000

    Fix: Consider local effector

--- trainer/preview.py
+++ trainer/preview.py
@@ -1,11 +1,13 @@
 from .simulation import create_scene, reset, apply_joints, render
 
+import flom
+
 import math
 
 EFFECTOR_SPHERE_RADIUS_RATIO = 0.05
 EFFECTOR_SPHERE_COLOR_RATIO = 1000
 
-def create_effector_visualizer(scene, parts, effectors):
+def create_effector_visualizer(scene, motion, robot, parts, effectors):
     def calc_color(differ):
         r =  - math.exp(-differ * EFFECTOR_SPHERE_COLOR_RATIO) + 1
         color_red = int(0xff * r)
@@ -13,10 +15,20 @@ def create_effector_visualizer(scene, parts, effectors):
         color = color_blue | color_red * (0xFFFF + 1)
         return color
 
-    def create(part, eff):
+    def select_pose(ty, pose, root):
+        if ty == flom.CoordinateSystem.World:
+            return pose
+        elif ty == flom.CoordinateSystem.Local:
+            return pose + root
+        else:
+            assert False  # unreachable
+
+    def create(name, eff):
+        part = parts[name]
+        ty = motion.effector_type(name)
         if eff.location:
             current = part.pose().xyz()
-            target = eff.location.vec
+            target = select_pose(ty.location, eff.location.vec, robot.root_part.pose().xyz())
             differ = sum((c - t) ** 2 for c, t in zip(current, target)) / 3
 
             color = calc_color(differ)
@@ -24,7 +36,8 @@ def create_effector_visualizer(scene, parts, effectors):
             x, y, z = target
             return scene.cpp_world.debug_sphere(x, y, z, radius, color)
 
-    return {name: create(parts[name], eff) for name, eff in effectors.items()}
+    robot.query_position()
+    return {name: create(name, eff) for name, eff in effectors.items()}
 
 def preview(motion, robot_file, timestep=0.0165/8, frame_skip=8):
     scene = create_scene(timestep, frame_skip)
@@ -37,6 +50,6 @@ def preview(motion, robot_file, timestep=0.0165/8, frame_skip=8):
 
         frame = motion.frame_at(scene.cpp_world.ts)
         apply_joints(joints, frame.positions)
-        effector_vis = create_effector_visualizer(scene, parts, frame.effectors)
+        effector_vis = create_effector_visualizer(scene, motion, robot, parts, frame.effectors)
 
         render(scene)
