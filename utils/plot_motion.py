aa41eedd83405287b258e6ccf73c61ec751a2ab0
coord.e <me@coord-e.com>
2019-01-07 00:34:09 +0000

    Add: Enable to configure number of loops in plot motion script

--- utils/plot_motion.py
+++ utils/plot_motion.py
@@ -7,6 +7,7 @@ import flom
 parser = ArgumentParser(description='Plot motion file')
 parser.add_argument('input', type=str, help='Input motion file')
 parser.add_argument('--fps', type=float, default=0.01)
+parser.add_argument('--loop', type=int, default=1)
 parser.add_argument('-o', '--output', type=str, help='output plot file', required=True)
 args = parser.parse_args()
 
@@ -17,14 +18,18 @@ ax = fig.add_subplot(1, 1, 1)
 
 joints = [j for j in motion.joint_names() if not j.startswith('ignore_')]
 
-def plot_frames(motion):
-    motion.set_loop(flom.LoopType.None_)
-    x = [i * args.fps for i, frame in enumerate(motion.frames(args.fps))]
-    ys = [frame.positions for frame in motion.frames(args.fps)]
+def plot_frames(motion, times):
+    x, ys = [], []
+    for i, frame in enumerate(motion.frames(args.fps)):
+        t = i * args.fps
+        x.append(t)
+        ys.append(frame.positions)
+        if i * args.fps > motion.length() * times:
+            break
     for joint in joints:
         ax.plot(x, [y[joint] for y in ys], label=joint)
 
-plot_frames(motion)
+plot_frames(motion, args.loop)
 
 ax.legend()
 plt.savefig(args.output)
