f879939d8e129b4eb08acb0590975cfd1eb7f0a7
coord.e <me@coord-e.com>
2019-01-16 17:00:50 +0000

    Add: Parameterize some coefficients in calc_reward function

--- trainer/evaluation.py
+++ trainer/evaluation.py
@@ -7,7 +7,7 @@ from .utils import select_location, select_rotation
 from .simulation import apply_joints
 
 
-def calc_reward(motion, robot, frame):
+def calc_reward(motion, robot, frame, k=1, wl=1, wr=0.001):
     diff = 0
     for name, effector in frame.effectors.items():
         pose = robot.link_state(name).pose
@@ -16,25 +16,24 @@ def calc_reward(motion, robot, frame):
         ty = motion.effector_type(name)
         if effector.location:
             target = select_location(ty.location, effector.location.vector, root_pose)
-            diff += np.linalg.norm(pose.vector - np.array(target)) ** 2 * weight.location
+            diff += wl * np.linalg.norm(pose.vector - np.array(target)) ** 2 * weight.location
         if effector.rotation:
             target = select_rotation(ty.rotation, effector.rotation.quaternion, root_pose)
             quat1 = np.quaternion(*target)
             quat2 = np.quaternion(*pose.quaternion)
-            diff += quaternion.rotation_intrinsic_distance(quat1, quat2) ** 2 * weight.rotation
-    k = 1
+            diff += wr * quaternion.rotation_intrinsic_distance(quat1, quat2) ** 2 * weight.rotation
     normalized = k * diff / len(frame.effectors)
     return - math.exp(normalized) + 1
 
 
-def evaluate(scene, motion, robot, loop=2):
+def evaluate(scene, motion, robot, loop=2, **kwargs):
     reward_sum = 0
     for t, frame in motion.frames(scene.dt):
         apply_joints(robot, frame.positions)
 
         scene.step()
 
-        reward_sum += calc_reward(motion, robot, frame)
+        reward_sum += calc_reward(motion, robot, frame, **kwargs)
 
         if t > motion.length() * loop:
             break
