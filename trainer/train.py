37c850d1a0fbf7eadf3dbf861896d7715250a40f
coord.e <me@coord-e.com>
2019-01-17 09:59:19 +0000

    Change: Entirely rewrite with Env

--- trainer/train.py
+++ trainer/train.py
@@ -59,32 +59,31 @@ class Env:
         self.state.restore(self.scene, self.robot)
 
 
-def train_chunk(motion: flom.Motion, scenes_robots: List[Tuple[Scene, Robot]], start: float, init_weights: np.ndarray, init_state: StateWithJoints, *, algorithm: str = 'OnePlusOne', num_iteration: int = 1000, weight_factor: float = 0.01, stddev: float = 1, **kwargs):
+def train_chunk(motion: flom.Motion, envs: List[Env], start: float, init_weights: np.ndarray, *, algorithm: str = 'OnePlusOne', num_iteration: int = 1000, weight_factor: float = 0.01, stddev: float = 1, **kwargs):
     weight_shape = np.array(init_weights).shape
 
-    thread_scene_robot = {}  # type: Dict[int, Tuple[Scene, Robot]]
+    thread_envs = {}  # type: Dict[int, Env]
 
-    def step(weights, scene_robot: Tuple[Scene, Robot] = None):
-        scene_robot = scene_robot or thread_scene_robot[threading.get_ident()]
+    def step(weights, env: Env = None):
+        env = env or thread_envs[threading.get_ident()]
 
-        scene, robot = scene_robot
-        init_state.restore(scene, robot)
+        env.restore()
 
         reward_sum = 0
-        start_ts = scene.ts
+        start_ts = env.scene.ts
 
-        pre_positions = try_get_pre_positions(scene, motion, start=start)
+        pre_positions = try_get_pre_positions(env.scene, motion, start=start)
 
         for init_weight, frame_weight in zip(init_weights, weights):
-            frame = motion.frame_at(start + scene.ts - start_ts)
+            frame = motion.frame_at(start + env.scene.ts - start_ts)
 
             frame.positions = apply_weights(
                 frame.positions, init_weight + frame_weight * weight_factor)
-            apply_joints(robot, frame.positions)
+            apply_joints(env.robot, frame.positions)
 
-            scene.step()
+            env.scene.step()
 
-            reward_sum += calc_reward(motion, robot, frame, pre_positions, **kwargs)
+            reward_sum += calc_reward(motion, env.robot, frame, pre_positions, **kwargs)
 
             pre_positions = frame.positions
 
@@ -93,32 +92,32 @@ def train_chunk(motion: flom.Motion, scenes_robots: List[Tuple[Scene, Robot]], s
     used_idx = 0
     lock = threading.Lock()
     def register_thread():
-        nonlocal used_idx, thread_scene_robot
+        nonlocal used_idx, thread_envs
         with lock:
-            thread_scene_robot[threading.get_ident()] = scenes_robots[used_idx]
+            thread_envs[threading.get_ident()] = envs[used_idx]
             used_idx += 1
 
     weights_param = Gaussian(mean=0, std=stddev, shape=weight_shape)
     inst_step = InstrumentedFunction(step, weights_param)
     optimizer = optimizerlib.registry[algorithm](
-        dimension=inst_step.dimension, budget=num_iteration, num_workers=len(scenes_robots))
+        dimension=inst_step.dimension, budget=num_iteration, num_workers=len(envs))
 
     with futures.ThreadPoolExecutor(max_workers=optimizer.num_workers, initializer=register_thread) as executor:
         recommendation = optimizer.optimize(inst_step, executor=executor)
     weights = np.reshape(recommendation, weight_shape)
 
-    scene, robot = scenes_robots[0]
-    reward = step(weights, (scene, robot))
+    for e in envs:
+        reward = step(weights, e)
+        e.save()
 
-    state = StateWithJoints.save(scene, robot)
-    return reward, weights * weight_factor, state
+    return reward, weights * weight_factor
 
 
 def train(motion: flom.Motion, make_scene: Callable[[int], Tuple[Scene, Robot]], *, num_workers: int = 5, chunk_length: int = 3, num_chunk: Optional[int] = None, **kwargs):
-    scenes_robots = [make_scene(i) for i in range(num_workers)]
-    first_scene, first_robot = scenes_robots[0]
+    envs = [Env(*make_scene(i)) for i in range(num_workers)]
+    first_env = envs[0]
 
-    chunk_duration = first_scene.dt * chunk_length
+    chunk_duration = first_env.scene.dt * chunk_length
 
     if num_chunk is None:
         num_chunk = math.ceil(motion.length() / chunk_duration)
@@ -131,13 +130,12 @@ def train(motion: flom.Motion, make_scene: Callable[[int], Tuple[Scene, Robot]],
     if total_length < motion.length():
         log.warning(f"A total length to train is shorter than the length of motion")
 
-    num_frames = int(motion.length() / first_scene.dt)
+    num_frames = int(motion.length() / first_env.scene.dt)
     num_joints = len(list(motion.joint_names()))  # TODO: Call len() directly
     weights = np.zeros(shape=(num_frames, num_joints))
     log.info(f"shape of weights: {weights.shape}")
     log.debug(f"kwargs: {kwargs}")
 
-    last_state = StateWithJoints.save(first_scene, first_robot)
     for chunk_idx in range(num_chunk):
         start = chunk_idx * chunk_duration
         start_idx = chunk_idx * chunk_length % num_frames
@@ -145,7 +143,7 @@ def train(motion: flom.Motion, make_scene: Callable[[int], Tuple[Scene, Robot]],
         r = range(start_idx, start_idx + chunk_length)
         in_weights = [weights[i % num_frames] for i in r]
         log.info(f"start training chunk {chunk_idx} ({start}~)")
-        reward, out_weights, last_state = train_chunk(motion, scenes_robots, start, in_weights, last_state, **kwargs)
+        reward, out_weights = train_chunk(motion, envs, start, in_weights, **kwargs)
         for i, w in zip(r, out_weights):
             weights[i % num_frames] = w
 
