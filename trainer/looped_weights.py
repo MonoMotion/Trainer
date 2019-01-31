2af7ed7c9af13529dbd6665dcf147b8b77c85010
coord.e <me@coord-e.com>
2019-01-31 03:31:31 +0000

    [WIP] Add: Add LoopedWeights to keep last and first frame identical

---
+++ trainer/looped_weights.py
@@ -0,0 +1,25 @@
+from collection.abc import MutableSequence
+import numpy as np
+
+
+class LoopedWeights(MutableSequence):
+    def __init__(self, num_frames: int, num_joints: int):
+        self.weights = np.zeros(shape=(num_frames - 1, num_joints))
+        self.size = num_frames
+
+    def __setitem__(self, key: int, value: np.ndarray):
+        idx: int = key % self.size
+        if idx == self.size - 1:
+            self.weights[0] = value
+        else:
+            self.weights[idx] = value
+
+    def __getitem__(self, key: int) -> np.ndarray:
+        idx: int = key % self.size
+        if idx == self.size - 1:
+            return self.weights[0]
+        else:
+            return self.weights[idx]
+
+    def __len__(self):
+        return self.size
